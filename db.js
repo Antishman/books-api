0a2ac58ebb282f7d886838a1d5d766fdf4bcbbd2
Anteneh <164549000+Antishman@users.noreply.github.com>
2024-11-27 14:44:13 +0300

    deployment

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:'); // In-memory database for simplicity
+const db = new sqlite3.Database('books.db'); 
 
 db.serialize(() => {
     db.run(`CREATE TABLE books (
